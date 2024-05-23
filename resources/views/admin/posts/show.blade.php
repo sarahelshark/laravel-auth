d59c0f278a0101373baab4b5fbf721f4afaa9453
sarahelshark <sarahelsh183@gmail.com>
2024-05-23 18:08:14 +0200

    [add]
    "go back " button to the main posts page

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,7 @@
    <img src="{{$post->cover_image}}" alt="">
    <div>{{$post->title}}</div>
    <div>{{$post->slug}}</div>
+   <a href="{{route('admin.posts.index')}}" > Go Back</a>
 </div>
 
 @endsection
