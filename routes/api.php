44bd3480c45bd0caeba00c499bd746b2ca5a08a4
sarahelshark <sarahelsh183@gmail.com>
2024-06-05 23:23:21 +0200

    [add]
    missing data for api

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,13 @@ use App\Models\Post;
 }); */
 
 //Oprion 2 - utilizzando response posso personalizzare la mia risposta json 
+//urilizzo eager loading  with()  per ovviare alla mancanza di informazioni user e category_id  > magari uso quello deldata per chiamataajax >>>QUINDI quando mi rispondi, non mi devi dare solo info inerenti al post ma anche categoria user e tag in una sola chiamata cosi le uso in lato front  (nomi metodi dei vari modelli dentro il modello come param di with )
+//http://127.0.0.1:8000/api/posts?page=2
 Route::get('posts',function(){
     return response()->json([
         'success' => true,
         'sarah' => 'test',
-        'results' => Post::orderByDesc('id')->paginate(), //->get()
+        'results' => Post::with(['category','user','tags'])->orderByDesc('id')->paginate(), //->get()
     ]);
 });
 
@@ -37,3 +39,22 @@ Route::get('posts',function(){
 //          http://127.0.0.1:80000/api/posts
 
 //c'e ancheun terzo modo per farlo in laravel, piu avanzato, cerco odpo
+
+
+//rotta singola
+Route::get('posts/{post}',function($id){
+    $post = Post::with(['category','user','tags'])->where('id', $id) ->first();
+    if($post){
+        return response()->json([
+            'success' => true,
+            'results' => $post
+        ]);
+
+    } else {
+        return response()->json([
+            'success' => false,
+            'results' => "404 not found"
+        ]);
+
+    }
+});
